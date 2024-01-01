01c9f5dc4189c10a284e10b501ba76d46ae7f5db
haidousm <haidous.m@gmail.com>
2023-12-31 21:04:13 -0800

    custom json unmarshaling of runtime field

--- internal/data/runtime.go
+++ internal/data/runtime.go
@@ -1,9 +1,13 @@
 package data
 
 import (
+	"errors"
 	"fmt"
+	"regexp"
 	"strconv"
 )
+
+var ErrInvalidRuntimeFormat = errors.New("invalid runtime format")
 
 type Runtime int32
 
@@ -12,4 +16,22 @@ func (r Runtime) MarshalJSON() ([]byte, error) {
 	quotedJSONValue := strconv.Quote(jsonValue)
 
 	return []byte(quotedJSONValue), nil
+}
+
+func (r *Runtime) UnmarshalJSON(jsonValue []byte) error {
+
+	re := regexp.MustCompile(`(?:(\d+) mins?"$)`)
+
+	matches := re.FindStringSubmatch(string(jsonValue))
+	if matches == nil || len(matches) > 2 {
+		return ErrInvalidRuntimeFormat
+	}
+
+	i, err := strconv.ParseInt(matches[1], 10, 32)
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	*r = Runtime(i)
+	return nil
 }
